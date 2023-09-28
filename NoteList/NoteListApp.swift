9d7d7385e105c31a0458fa254492a92039f4070a
Pablo E Diaz <pdramirez.h@gmail.com>
2023-09-27 22:19:38 -0400

    Initial Commit

---
+++ NoteList/NoteListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteListApp.swift
+//  NoteList
+//
+//  Created by Pablo E Diaz Ramairez on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
